8bd49958323a7c61abc121f90e83544ee2336597
Ghadah <ghadahamdan451@gmail.com>
2026-03-14 07:08:25 +0300

    add char+animation

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,36 +5,30 @@ public class PlayerMovement : MonoBehaviour
     private float horizontal;
     private float speed = 8f;
     private float jump = 16f;
-    private bool isFacingRight = true;
-
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer sprite;
 
-    // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
+        // الحركة
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+
+        // القفز
         if(Input.GetButtonDown("Jump") && IsGrounded())
-        {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jump);
-        }
 
-         if(Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
-        }
-
+        // Flip الشخصية
         Flip();
 
-    }
-
-
-    private void FixedUpdate()
-    {
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        // تحديث الأنيميشن
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+        animator.SetBool("IsJumping", !IsGrounded());
     }
 
     private bool IsGrounded()
@@ -42,14 +36,11 @@ public class PlayerMovement : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
-
     private void Flip()
     {
-        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f){
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
+        if(horizontal > 0)
+            sprite.flipX = false;
+        else if(horizontal < 0)
+            sprite.flipX = true;
     }
 }
